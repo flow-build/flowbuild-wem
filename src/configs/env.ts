f25e1db0cb155f828e1db1def5c19f5e90630666
imagure <ricardo.imagure@gmail.com>
2023-04-04 16:12:46 -0300

    fix: adjust default envs

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -4,7 +4,7 @@ const envs = {
   FS_TOKEN_EXPIRE: parseInt(process.env.FS_TOKEN_TTL || '300', 10),
   STREAM_CONFIG: JSON.parse(
     process.env.STREAM_CONFIG ||
-    `
+      `
     {
       "topics": {
         "wem-start-process": {
@@ -14,8 +14,7 @@ const envs = {
     }
   `
   ),
-  BROKER_HOST:
-    process.env.BROKER_HOST || 'localhost',
+  BROKER_HOST: process.env.BROKER_HOST || 'localhost',
   BROKER_PORT: process.env.BROKER_PORT || '9092',
   BROKER_KAFKA_MECHANISM: process.env.BROKER_KAFKA_MECHANISM,
   BROKER_KAFKA_USERNAME: process.env.BROKER_KAFKA_USERNAME,
