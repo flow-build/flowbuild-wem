3f740d22f7116e34bca8c8e9007981d3a7b66448
imagure <ricardo.imagure@gmail.com>
2023-04-04 15:49:48 -0300

    feat: adds kafka sasl optional configs

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -14,11 +14,21 @@ const envs = {
     }
   `
   ),
-  BROKER_HOST: process.env.BROKER_HOST || 'localhost',
+  BROKER_HOST:
+    process.env.BROKER_HOST || 'pkc-n00kk.us-east-1.aws.confluent.cloud',
   BROKER_PORT: process.env.BROKER_PORT || '9092',
+  BROKER_KAFKA_MECHANISM: process.env.BROKER_KAFKA_MECHANISM,
+  BROKER_KAFKA_USERNAME: process.env.BROKER_KAFKA_USERNAME,
+  BROKER_KAFKA_SECRET: process.env.BROKER_KAFKA_SECRET,
   REDIS_PASSWORD:
     process.env.REDIS_PASSWORD || 'eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81',
   REDIS_HOST: process.env.REDIS_HOST || 'localhost',
   REDIS_PORT: process.env.REDIS_PORT || '6379',
 }
 export { envs }
+
+// KAFKA_BOOTSTRAP_SERVER=pkc-n00kk.us-east-1.aws.confluent.cloud:9092
+// KAFKA_SEC_PROTOCOL=SASL_SSL
+// KAFKA_SASL_MECHANISMS=PLAIN
+// KAFKA_CLUSTER_API_KEY=W2CJ3ONMAG2FVSD7
+// KAFKA_API_SECRET=BDx1vlPtBJw97+q6rZrQ97vvsK8AjU8l6R2MMu1C5KDM4W9wAxicgVSLywSrIevO
--- src/stream/kafka/client.ts
+++ src/stream/kafka/client.ts
@@ -1,4 +1,4 @@
-import { Consumer, Kafka, Producer } from 'kafkajs'
+import { Consumer, Kafka, KafkaConfig, Producer, SASLOptions } from 'kafkajs'
 import { v4 as uuid } from 'uuid'
 import { envs } from '@configs/env'
 
@@ -7,10 +7,28 @@ let producer: Producer
 let consumer: Consumer
 
 const setClient = () => {
-  client = new Kafka({
+  let sasl
+  const mechanism = envs.BROKER_KAFKA_MECHANISM
+  const username = envs.BROKER_KAFKA_USERNAME
+  const pswd = envs.BROKER_KAFKA_SECRET
+
+  const connectionConfig = {
     clientId: `wem-${uuid()}`,
     brokers: [`${envs.BROKER_HOST}:${envs.BROKER_PORT}`],
-  })
+  } as KafkaConfig
+
+  if (mechanism && username && pswd) {
+    sasl = {
+      mechanism,
+      username,
+      password: pswd,
+    } as SASLOptions
+
+    connectionConfig.sasl = sasl
+    connectionConfig.ssl = !!sasl
+  }
+
+  client = new Kafka(connectionConfig)
 
   producer = client.producer()
   consumer = client.consumer({
