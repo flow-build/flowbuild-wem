d1abf7caeffb0ce7a6ef191a3349056fcf93b6c6
imagure <ricardo.imagure@gmail.com>
2023-04-04 16:12:36 -0300

    fix: adjust default envs

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -4,7 +4,7 @@ const envs = {
   FS_TOKEN_EXPIRE: parseInt(process.env.FS_TOKEN_TTL || '300', 10),
   STREAM_CONFIG: JSON.parse(
     process.env.STREAM_CONFIG ||
-      `
+    `
     {
       "topics": {
         "wem-start-process": {
@@ -15,7 +15,7 @@ const envs = {
   `
   ),
   BROKER_HOST:
-    process.env.BROKER_HOST || 'pkc-n00kk.us-east-1.aws.confluent.cloud',
+    process.env.BROKER_HOST || 'localhost',
   BROKER_PORT: process.env.BROKER_PORT || '9092',
   BROKER_KAFKA_MECHANISM: process.env.BROKER_KAFKA_MECHANISM,
   BROKER_KAFKA_USERNAME: process.env.BROKER_KAFKA_USERNAME,
@@ -26,9 +26,3 @@ const envs = {
   REDIS_PORT: process.env.REDIS_PORT || '6379',
 }
 export { envs }
-
-// KAFKA_BOOTSTRAP_SERVER=pkc-n00kk.us-east-1.aws.confluent.cloud:9092
-// KAFKA_SEC_PROTOCOL=SASL_SSL
-// KAFKA_SASL_MECHANISMS=PLAIN
-// KAFKA_CLUSTER_API_KEY=W2CJ3ONMAG2FVSD7
-// KAFKA_API_SECRET=BDx1vlPtBJw97+q6rZrQ97vvsK8AjU8l6R2MMu1C5KDM4W9wAxicgVSLywSrIevO
