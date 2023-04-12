a11dcc532b917bf9dc01a29dd2e6e653cd3ca21a
imagure <ricardo.imagure@gmail.com>
2023-04-12 14:53:52 -0300

    feat: adds new types

--- src/types/start_process.type.ts
+++ src/types/start_process.type.ts
@@ -1,6 +1,22 @@
 import { LooseObject } from './LooseObject.type'
 
+export type TopicMap = {
+  workflow_name: string
+}
+
+export type BaseMessage = {
+  process_input: LooseObject
+  process_id?: string
+  workflow_name?: string
+}
+
 export type StartProcessMessage = {
   workflow_name: string
   process_input: LooseObject
+}
+
+export type ContinueProcessMessage = {
+  process_id?: string
+  process_input: LooseObject
+  workflow_name?: string
 }
