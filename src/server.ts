b93ef9c259ef31390680c53109d8e1e1b2ad3350
imagure <ricardo.imagure@gmail.com>
2023-04-19 14:17:42 -0300

    feat: updates app initializing considering REDIS usage

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,11 @@ import { envs } from '@configs/env'
 import { runWorker } from './workerRunner'
 
 const runServer = async () => {
-  const workerEntities = await runWorker()
+  await runWorker()
 
-  const server = await app(
-    {
-      logger: true,
-    },
-    workerEntities
-  )
+  const server = await app({
+    logger: true,
+  })
 
   server.listen({ port: envs.SERVER_PORT, host: '0.0.0.0' }, (err, address) => {
     if (err) throw err
